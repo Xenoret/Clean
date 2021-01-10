5164a9bb1cd18f39acea602221125418cf849f9f
Xenoret <63548161+Xenoret@users.noreply.github.com>
2021-01-10 08:16:26 +0000

    Create clean.py

---
+++ clean.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#-*- coding: UTF-8 -*-
+
+#by Xenoret
+
+import os
+
+os.system('cls || clear')
+
+print('''
+========================
+{FAST CLEAR YOUR SYSTEM}
+=======================''')
+
+os.system('sudo apt-get clean && sudo apt-get autoremove')
+
+print('''\n
+======================================
+{Your operating system is clean, bye!}
+======================================''')
