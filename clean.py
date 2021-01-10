9caba4661492fc65a49ed8c1d47f266a489689dc
Xenoret <63548161+Xenoret@users.noreply.github.com>
2021-01-10 08:48:40 +0000

    Update clean.py

--- clean.py
+++ clean.py
@@ -3,6 +3,7 @@
 
 #by Xenoret
 
+
 import os
 
 os.system('cls || clear')
@@ -12,7 +13,8 @@ print('''
 {FAST CLEAR YOUR SYSTEM}
 =======================''')
 
-os.system('sudo apt-get clean && sudo apt-get autoremove')
+os.system('sudo apt-get clean && sudo apt-get autoremove')#Эти команды очистят локальный репозиторий от скачанных файлов пакетов.
+os.system('sudo apt -f install')#Установка пакетов зависимостей которые были не до конца установлены
 
 print('''\n
 ======================================
